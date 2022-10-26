98f0929bcdb142dcc4c8f7070d1c08cfcc0550d7
Stijn Lievens <stijn.lievens@hogent.be>
2022-10-26 10:15:05 +0200

    Twee testen CR toevoegen

--- test/backend/RuleFinding/CR/test_columnrule.py
+++ test/backend/RuleFinding/CR/test_columnrule.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from visions import Count
+import numpy as np
 from backend.RuleFinding.CR.ColumnRule import ColumnRule
 
 
@@ -31,6 +31,9 @@ def test_columnrule_creation():
     assert df_to_be_corrected['SUGGEST_CON'].equals(pd.Series(["b1", "b1", "b2"]))
     assert df_to_be_corrected['RULESTRING'].equals(pd.Series([rule_string, rule_string, rule_string]))
 
+    # Check confidence
+    assert cr.confidence == 0.0
+
 
 def test_columnrule_creation_bis():
     # Test that correct rows do not appear in the dataframe to correct    
@@ -59,3 +62,59 @@ def test_columnrule_creation_bis():
         equals(pd.Series(["b1", "b1", "b2"])))
     assert (df_to_be_corrected['RULESTRING'].reset_index(drop=True).
         equals(pd.Series([rule_string, rule_string, rule_string])))
+
+
+def test_predict():
+    rule_string = "A => B"
+
+    value_mapping = {
+        frozenset(["A_a1"]) : "B_b1",
+        frozenset(["A_a2"]) : "B_b2",
+    }
+
+    df = pd.DataFrame(
+        {'A' : ["a1", "a1", "a2"],
+         'B' : ["x", "y", "z"],
+         'C' : ["c", "c", "c"]
+        }
+    )
+
+    cr = ColumnRule(rule_string = rule_string, value_mapping = value_mapping, original_df = df)
+
+    predicted_values = cr.predict(df)
+
+    assert predicted_values.shape == (df.shape[0], 2)
+    assert (predicted_values['A'].reset_index(drop=True).
+        equals(pd.Series(["a1", "a1", "a2"])))
+    assert (predicted_values['B'].reset_index(drop=True).
+        equals(pd.Series(["b1", "b1", "b2"])))
+
+def test_status():
+    rule_string = "A => B"
+
+    value_mapping = {
+        frozenset(["A_a1"]) : "B_b1",
+        frozenset(["A_a2"]) : "B_b2",
+    }
+
+    df = pd.DataFrame(
+        {'A' : ["a1", "a1", "a2"],
+         'B' : ["x", "y", "z"],
+         'C' : ["c", "c", "c"]
+        }
+    )
+
+    cr = ColumnRule(rule_string = rule_string, value_mapping = value_mapping, original_df = df)
+
+    df_for_status =  pd.DataFrame(
+        {'A' : ["a1", "a1", "a2", "a2"],
+         'B' : ["x", "b1", "b2", "y"],
+         'C' : ["c", "c", "c", "c"]
+        }
+    )
+
+    status = cr.status(df_for_status)
+
+    assert np.array_equal(status, np.array([-1,1,1,-1]))
+
+
