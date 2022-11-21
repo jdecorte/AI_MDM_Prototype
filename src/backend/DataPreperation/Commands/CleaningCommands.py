751ef4029969795820b5f66312f985da86e146f9
Stijn Lievens <stijn.lievens@hogent.be>
2022-11-21 21:03:07 +0100

    CleaningCommands: make linter happy

--- src/backend/DataPreperation/Commands/CleaningCommands.py
+++ src/backend/DataPreperation/Commands/CleaningCommands.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
-
 import pandas as pd
-import numpy as np
 import re
+
 
 class CleaningCommand(ABC):
 
@@ -15,8 +14,7 @@ class CleaningCommand_Trim(CleaningCommand):
 
     def __init__(self, series: pd.Series) -> None:
         self.series = series
-        
- 
+
     def execute(self) -> pd.Series:
         return self.series.apply(lambda x: str.strip(str(x)))
 
@@ -27,14 +25,14 @@ class CleaningCommand_StringToFloat(CleaningCommand):
 
     def __init__(self, series: pd.Series) -> None:
         self.series = series
-        
- 
+
     def execute(self) -> pd.Series:
-        return self.series.apply(lambda stringValue: self._extract_float_from_string(stringValue))
+        return self.series.apply(
+            lambda stringValue: self._extract_float_from_string(stringValue))
 
     def _extract_float_from_string(self, s: str) -> float:
         lst = re.findall(r"[-+]?\d*\.\d+|\d+", str(s))
-        if len(lst)> 0:
+        if len(lst) > 0:
             return max([float(i) for i in lst])
         else:
             return pd.NA
