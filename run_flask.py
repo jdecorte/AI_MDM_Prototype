ce28cad44190400b85676d1b9bbbccbfae316237
Stijn Lievens <stijn.lievens@hogent.be>
2023-01-12 14:07:53 +0000

    Aanpassen run_flask for gunicorn

--- run_flask.py
+++ run_flask.py
@@ -1,12 +1,16 @@
 from src.backend.DomainController import DomainController
 from flask import Flask
 
+
+
 def main():
     app = Flask(__name__)
     dc = DomainController(app=app)
     DomainController.register(app, route_base="/")
-    dc.run_flask()
+    # dc.run_flask()
     return app
+
+app = main()
 
 if __name__ == "__main__":
     main()
