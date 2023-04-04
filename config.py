ef2a8f497d28afbe4766a80bb2ebb40629a306bb
Stijn Lievens <stijn.lievens@hogent.be>
2023-04-04 11:29:40 +0200

    Save Dataprep.EDA report to a file

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import logging
 import logging.handlers
-import os
 
 
 PATH = "./rule_finder.log"
-
-
 logger = logging.getLogger("aimdm")
 logger.setLevel(logging.DEBUG)
-
-
-
 handler = logging.handlers.RotatingFileHandler(PATH, mode='a')
-formatter = logging.Formatter('RuleFinder | {funcName} | {asctime} | {levelname:8s} | {message}',datefmt='%Y-%m-%d %H:%M:%S',style='{')
+formatter = logging.Formatter(
+    'RuleFinder | {funcName} | {asctime} | {levelname:8s} | {message}',
+    datefmt='%Y-%m-%d %H:%M:%S', style='{')
 handler.setFormatter(formatter)
 
-
 logger.addHandler(handler)
+
+configuration = {
+    # Data profiling report will be stored in the following directory under "reports"
+    "WWW_ROOT": "/usr/share/nginx/html",
+}
--- src/frontend/Profiler/ProfilerInitPage.py
+++ src/frontend/Profiler/ProfilerInitPage.py
@@ -3,9 +3,13 @@ from streamlit_pandas_profiling import st_profile_report
 from dataprep.eda import create_report
 from src.frontend.Handler.IHandler import IHandler
 from ydata_profiling import ProfileReport
+import hashlib
+import os
+import config as cfg
 
 
 class ProfilerInitPage:
+
     def __init__(self, canvas, handler: IHandler) -> None:
         self.canvas = canvas
         self.handler = handler
@@ -16,5 +20,25 @@ class ProfilerInitPage:
         st_profile_report(pr)
 
     def show_dataprep_profiling(self):
-        st.header('Data Profiling Report is succesvol gegenereerd in een nieuw tabblad')
-        create_report(st.session_state["dataframe"]).show_browser()
+
+        # Create MD5 of the dataframe to use as a filename
+        df = st.session_state["dataframe"]
+        md5_hash = hashlib.md5(df.to_string().encode("utf-8")).hexdigest()
+
+        # Make path to directory to store the reports.
+        # We assume that this directory already exists.
+        directory = os.path.join(cfg.configuration["WWW_ROOT"], "reports")
+
+        # Create and save the report
+        path = os.path.join(directory, f"{md5_hash}.html")
+        report = create_report(df)
+        report.save(path)
+
+        # Show a link to the report
+        text = "Link to the report"
+        st.header('Data Profiling Report is gegenereerd.' +
+                  'Klik op de link hieronder om het te bekijken')
+        # The url starts from WWW_ROOT, so we need to go up one level
+        # in order to get rid of "aimdmtool" in the url
+        url = os.path.join("..", "reports", f"{md5_hash}.html")
+        st.markdown(f'<a href="{url}">{text}</a>', unsafe_allow_html=True)
