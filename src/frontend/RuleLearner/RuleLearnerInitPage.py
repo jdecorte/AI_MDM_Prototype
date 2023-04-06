ba5e2b7a654a312ffda6813a08d3daee35cf388d
Stijn Lievens <stijn.lievens@hogent.be>
2023-04-06 16:56:49 +0200

    Make linter happy on RuleLearner pages in frontend

--- src/frontend/RuleLearner/RuleLearnerInitPage.py
+++ src/frontend/RuleLearner/RuleLearnerInitPage.py
@@ -1,19 +1,14 @@
 import streamlit as st
-import pandas as pd
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
+from st_aggrid import GridOptionsBuilder, AgGrid
 from src.frontend.RuleLearner.RuleLearnerOptionsComponent import RuleLearnerOptionsComponent
 from src.frontend.Handler.IHandler import IHandler
-
-from src.shared.Enums.FiltererEnum import FiltererEnum
-from src.shared.Enums.BinningEnum import BinningEnum
-from src.shared.Enums.DroppingEnum import DroppingEnum
-
 from src.shared.Configs.RuleFindingConfig import RuleFindingConfig
-from src.shared.Views.ColumnRuleView import ColumnRuleView
 
 from src.frontend.DatasetDisplayer.DatasetDisplayerComponent import DatasetDisplayerComponent
 
+
 class RuleLearnerInitPage:
+
     def __init__(self, canvas, handler: IHandler) -> None:
         self.canvas = canvas
         self.handler = handler
@@ -31,30 +26,38 @@ class RuleLearnerInitPage:
         return d
 
     def _show_ag_grid(self):
-            # Toon de dataframe -> NIET EDITEERBAAR
-            MIN_HEIGHT = 50
-            MAX_HEIGHT = 500
-            ROW_HEIGHT = 60
+        # Toon de dataframe -> NIET EDITEERBAAR
+        MIN_HEIGHT = 50
+        MAX_HEIGHT = 500
+        ROW_HEIGHT = 60
 
-            st.markdown(f"<h4>Ingeladen Dataset: </h4>", unsafe_allow_html=True)
-            gb = GridOptionsBuilder.from_dataframe(st.session_state["dataframe"])
-            gb.configure_side_bar()
-            gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=False)
-            gridOptions = gb.build()
-            grid_response = AgGrid(st.session_state["dataframe"], gridOptions=gridOptions, enable_enterprise_modules=True, height=min(MIN_HEIGHT + len(st.session_state["dataframe"]) * ROW_HEIGHT, MAX_HEIGHT))
+        st.markdown("<h4>Ingeladen Dataset: </h4>", unsafe_allow_html=True)
+        gb = GridOptionsBuilder.from_dataframe(st.session_state["dataframe"])
+        gb.configure_side_bar()
+        gb.configure_default_column(
+            groupable=True,
+            value=True,
+            enableRowGroup=True,
+            aggFunc="sum",
+            editable=False)
+        gridOptions = gb.build()
+        grid_response = AgGrid(
+            st.session_state["dataframe"],
+            gridOptions=gridOptions,
+            enable_enterprise_modules=True,
+            height=min(MIN_HEIGHT + len(st.session_state["dataframe"]) * ROW_HEIGHT, MAX_HEIGHT))
 
-    def show(self): 
-        with self.canvas.container(): 
-
+    def show(self):
+        with self.canvas.container():
             st.title("Rule Learning")
             DatasetDisplayerComponent().show(st.session_state["dataframe"])
             RuleLearnerOptionsComponent().show()
 
             if st.button("Analyseer Data"):
                 rule_finding_config = RuleFindingConfig(
-                    rule_length=st.session_state["rule_length"], 
+                    rule_length=st.session_state["rule_length"],
                     min_support=st.session_state["min_support"],
-                    lift=st.session_state["lift"], 
+                    lift=st.session_state["lift"],
                     confidence=st.session_state["confidence"],
                     filtering_string=st.session_state["filtering_string"],
                     dropping_options=st.session_state["dropping_options"],
@@ -63,6 +66,9 @@ class RuleLearnerInitPage:
                 json_rule_finding_config = rule_finding_config.to_json()
 
                 # Set session_state attributes
-                st.session_state['gevonden_rules_dict'] = self.handler.get_column_rules(dataframe_in_json=st.session_state["dataframe"].to_json(),rule_finding_config_in_json=json_rule_finding_config, seq=st.session_state["current_seq"])
+                st.session_state['gevonden_rules_dict'] = self.handler.get_column_rules(
+                    dataframe_in_json=st.session_state["dataframe"].to_json(),
+                    rule_finding_config_in_json=json_rule_finding_config,
+                    seq=st.session_state["current_seq"])
                 st.session_state["currentState"] = "BekijkRules"
                 st.experimental_rerun()
--- src/frontend/RuleLearner/RuleLearnerOptionsComponent.py
+++ src/frontend/RuleLearner/RuleLearnerOptionsComponent.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import extra_streamlit_components as stx
-import pandas as pd
-from src.frontend.Handler.IHandler import IHandler
 
 from src.shared.Enums.FiltererEnum import FiltererEnum
 from src.shared.Enums.BinningEnum import BinningEnum
 from src.shared.Enums.DroppingEnum import DroppingEnum
 
-from src.shared.Configs.RuleFindingConfig import RuleFindingConfig
-from src.shared.Views.ColumnRuleView import ColumnRuleView
 
 class RuleLearnerOptionsComponent:
+
     def __init__(self) -> None:
         pass
 
@@ -26,8 +23,7 @@ class RuleLearnerOptionsComponent:
     def _create_default_dropping_dict(_self, d):
         return d
 
-    def show(self): 
-                
+    def show(self):
         # # Default values:
         default_rule_length = 3
         default_min_support = 0.0001
@@ -39,11 +35,13 @@ class RuleLearnerOptionsComponent:
 
         preview_default_to_show = self._create_default_dropping_dict(default_dropping_options)
         if "dropping_options" in st.session_state:
-            preview_total_to_show = self._create_total_dropping_dict(st.session_state["dropping_options"])
+            preview_total_to_show = self._create_total_dropping_dict(
+                st.session_state["dropping_options"])
         else:
             preview_total_to_show = self._create_total_dropping_dict({})
         if "binning_option" in st.session_state:
-            preview_total_to_show_binning = self._create_total_binning_dict(st.session_state["binning_option"])
+            preview_total_to_show_binning = self._create_total_binning_dict(
+                st.session_state["binning_option"])
         else:
             preview_total_to_show_binning = self._create_total_binning_dict({})
         # # END DEFAULTS
@@ -54,21 +52,45 @@ class RuleLearnerOptionsComponent:
             stx.TabBarItemData(id=2, title="Dropping", description=""),
             stx.TabBarItemData(id=3, title="Binning", description=""),
             ], default=1)
-        
+
         # Algoritme
         if chosen_tab == "1":
-            st.number_input('Rule length:', value=default_rule_length, format="%d", key="rule_length")
-            st.slider('Minimum support', min_value=0.0, max_value=1.0, step=0.0001, value=default_min_support, key="min_support" )
-            st.slider('Minimum lift', 0.0, 10.0, default_lift, key="lift")
-            st.slider('Minimum confidence', 0.0, 1.0, default_confidence, key="confidence")
-            st.selectbox('Filtering Type:', [e.value for e in FiltererEnum] , index=[e.value for e in FiltererEnum].index(default_filtering_string), key="filtering_string")
+            st.number_input(
+                'Rule length:',
+                value=default_rule_length,
+                format="%d",
+                key="rule_length")
+            st.slider(
+                'Minimum support',
+                min_value=0.0,
+                max_value=1.0,
+                step=0.0001,
+                value=default_min_support,
+                key="min_support")
+            st.slider(
+                'Minimum lift',
+                min_value=0.0,
+                max_value=10.0,
+                value=default_lift,
+                key="lift")
+            st.slider(
+                'Minimum confidence',
+                min_value=0.0,
+                max_value=1.0,
+                value=default_confidence,
+                key="confidence")
+            st.selectbox(
+                'Filtering Type:',
+                [e.value for e in FiltererEnum],
+                index=[e.value for e in FiltererEnum].index(default_filtering_string),
+                key="filtering_string")
 
         # Dropping
         if chosen_tab == "2":
-            colA, colB,_, colC = st.columns([3,4,1,8])
+            colA, colB, _, colC = st.columns([3, 4, 1, 8])
             with colB:
-                v = st.selectbox('Default Voorwaarde:', [e.value for e in DroppingEnum] )
-                w = st.text_input("Waarde") 
+                v = st.selectbox('Default Voorwaarde:', [e.value for e in DroppingEnum])
+                w = st.text_input("Waarde")
 
                 colA_1, colB_1 = st.columns(2)
                 with colA_1:
@@ -84,17 +106,21 @@ class RuleLearnerOptionsComponent:
 
             with colC:
                 st.subheader("Kolomspecifieke Dropping Options:")
-                
-                kolom_specific= None
+
+                kolom_specific = None
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    kolom_specific = st.selectbox('Kolom:', [e for e in st.session_state["dataframe"].columns] )
+                    kolom_specific = st.selectbox(
+                        'Kolom:',
+                        [e for e in st.session_state["dataframe"].columns])
                 with col2:
-                    vw_specific = st.selectbox('Voorwaarde:', [e.value for e in DroppingEnum] )
+                    vw_specific = st.selectbox(
+                        'Voorwaarde:',
+                        [e.value for e in DroppingEnum])
                 with col3:
                     value_specific = st.text_input("Value")
 
-                colC_1, colC_2,_ = st.columns([4,4,14])
+                colC_1, colC_2, _ = st.columns([4, 4, 14])
                 with colC_1:
                     buttonC_1 = st.button("Add Voorwaarde")
                     if buttonC_1:
@@ -106,33 +132,36 @@ class RuleLearnerOptionsComponent:
                     buttonC_2 = st.button("Remove Voorwaarde")
                     if buttonC_2:
                         if vw_specific and kolom_specific:
-                            del preview_total_to_show[kolom_specific][vw_specific]    
+                            del preview_total_to_show[kolom_specific][vw_specific]
 
             with colA:
-                st.subheader("Default Dropping Options:")                    
+                st.subheader("Default Dropping Options:")
                 st.write(preview_default_to_show)
 
-                use_default = st.checkbox('Maak gebruik van default voorwaarden', value=True)
-                temp_dict = {key: preview_default_to_show.copy() for key in st.session_state["dataframe"].columns}
+                use_default = st.checkbox(
+                    'Maak gebruik van default voorwaarden',
+                    value=True)
+                temp_dict = {key: preview_default_to_show.copy()
+                             for key in st.session_state["dataframe"].columns}
                 if use_default:
                     if preview_total_to_show is None:
                         preview_total_to_show = self._create_total_dropping_dict({})
-                    
-                    for k,v in temp_dict.items():
+
+                    for k, v in temp_dict.items():
                         for v1, v2 in v.items():
                             if k not in preview_total_to_show:
                                 preview_total_to_show[k] = {}
                             if v1 not in preview_total_to_show:
                                 preview_total_to_show[k][v1] = {}
                             preview_total_to_show[k][v1] = v2
-                    
+
                 else:
                     # Nu opnieuw de values uit temp_dict eruit gooien
-                    for k,v in temp_dict.items():
-                        for v1,v2 in v.items():
+                    for k, v in temp_dict.items():
+                        for v1, v2 in v.items():
                             if k not in preview_total_to_show:
                                 preview_total_to_show[k] = {}
-                            preview_total_to_show[k].pop(v1, None)                        
+                            preview_total_to_show[k].pop(v1, None)
 
             st.subheader("Opties die zullen worden toegepast:")
             st.write(preview_total_to_show)
@@ -142,31 +171,40 @@ class RuleLearnerOptionsComponent:
             colA_binning, colB_binning = st.columns(2)
             with colA_binning:
                 st.subheader("Default Binning Option:")
-                
-                default_binning_option = st.selectbox('Binning methode:', [e.value for e in BinningEnum], key="kolom_default_binning")                    
-                use_default_binning = st.checkbox('Maak gebruik van default voorwaarden', value=False, key="checkbox_default_binning")
-                temp_dict_binning = {key: default_binning_option for key in st.session_state["dataframe"].columns}
 
+                default_binning_option = st.selectbox(
+                    'Binning methode:',
+                    [e.value for e in BinningEnum], key="kolom_default_binning")                    
+                use_default_binning = st.checkbox(
+                    'Maak gebruik van default voorwaarden',
+                    value=False,
+                    key="checkbox_default_binning")
+                temp_dict_binning = {key: default_binning_option
+                                     for key in st.session_state["dataframe"].columns}
 
                 if use_default_binning:
-                    for k,v in temp_dict_binning.items():
+                    for k, v in temp_dict_binning.items():
                         preview_total_to_show_binning[k] = v
-                    
                 else:
-                    for k,v in temp_dict_binning.items():
+                    for k, v in temp_dict_binning.items():
                         if k in preview_total_to_show_binning:
                             del preview_total_to_show_binning[k]
 
             with colB_binning:
                 st.subheader("Kolomspecifieke Binning Options:")
-                kolom_specific_binnig= None
+                kolom_specific_binnig = None
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    kolom_specific_binnig = st.selectbox('Kolom:', [e for e in st.session_state["dataframe"].columns], key="Kolom_Binning")
+                    kolom_specific_binnig = st.selectbox(
+                        'Kolom:',
+                        [e for e in st.session_state["dataframe"].columns],
+                        key="Kolom_Binning")
                 with col2:
-                    specific_binnig = st.selectbox('Binning methode:', [e.value for e in BinningEnum] )
-                
-                colC_1_binning, colC_2_binning,_ = st.columns([4,4,14])
+                    specific_binnig = st.selectbox(
+                        'Binning methode:',
+                        [e.value for e in BinningEnum])
+
+                colC_1_binning, colC_2_binning, _ = st.columns([4, 4, 14])
                 with colC_1_binning:
                     buttonC_1_binning = st.button("Add Binning")
                     if buttonC_1_binning:
