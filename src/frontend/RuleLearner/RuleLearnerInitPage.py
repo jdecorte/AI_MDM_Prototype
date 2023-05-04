4a4ea9766a06aa948120f73c9ffea40fe5b4627f
MarioVerstraeten <MarioVerstraeten@outlook.com>
2023-05-04 11:08:07 +0200

    bug fix, key error for session state rule length

--- src/frontend/RuleLearner/RuleLearnerInitPage.py
+++ src/frontend/RuleLearner/RuleLearnerInitPage.py
@@ -114,32 +114,27 @@ class RuleLearnerInitPage:
                 st.session_state["rule_length"] = st.number_input(
                     'Rule length:',
                     value=default_rule_length,
-                    format="%d",
-                    key="rule_length")
+                    format="%d")
                 st.session_state["min_support"] = st.slider(
                     'Minimum support',
                     min_value=0.0,
                     max_value=1.0,
                     step=0.0001,
-                    value=default_min_support,
-                    key="min_support")
+                    value=default_min_support)
                 st.session_state["lift"] = st.slider(
                     'Minimum lift',
                     min_value=0.0,
                     max_value=10.0,
-                    value=default_lift,
-                    key="lift")
+                    value=default_lift)
                 st.session_state["confidence"] = st.slider(
                     'Minimum confidence',
                     min_value=0.0,
                     max_value=1.0,
-                    value=default_confidence,
-                    key="confidence")
+                    value=default_confidence)
                 st.session_state["filtering_string"] = st.selectbox(
                     'Filtering Type:',
                     [e.value for e in FiltererEnum],
-                    index=[e.value for e in FiltererEnum].index(default_filtering_string),
-                    key="filtering_string")
+                    index=[e.value for e in FiltererEnum].index(default_filtering_string))
 
             # Dropping
             if chosen_tab == "2":
